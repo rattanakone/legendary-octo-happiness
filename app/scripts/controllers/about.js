d8500776084befae62e5e06b3453e87d683eeb88
rattanakone <rattanakone@users.noreply.github.com>
2016-02-02 18:49:52 -0800

    resources and workaround on imagemin

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name legendaryOctoHappinessApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the legendaryOctoHappinessApp
+ */
+angular.module('legendaryOctoHappinessApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name legendaryOctoHappinessApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the legendaryOctoHappinessApp
+ */
+angular.module('legendaryOctoHappinessApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
